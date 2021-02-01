c8f1eef75c7d5a75cfc9f19cf6606d833662256d
costas <constantinos.chatzis@nodalpoint.com>
2021-02-01 18:29:17 +0200

    Update Image pre-processor

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,6 +1,8 @@
 import PIL.Image
 import torchvision.transforms as transforms
 import torch
+import jetson.utils
+import numpy
 
 
 class ImagePreProcessor:
@@ -9,12 +11,25 @@ class ImagePreProcessor:
         self.width = width
 
     def preprocessImage(self, image):
-        image = PIL.Image.fromarray(image)
+        # allocate the output, with half the size of the input
+        imgOutput = jetson.utils.cudaAllocMapped(width=self.width,
+                                                 height=self.height,
+                                                 format=image.format)
+
+        # rescale the image (the dimensions are taken from preprocessor)
+        jetson.utils.cudaResize(image, imgOutput)
+
+        # convert to numpy Array (RGBA)
+        array = jetson.utils.cudaToNumpy(imgOutput).astype(numpy.uint8)
+        # Get only 3 channels RGB
+        image = PIL.Image.fromarray(array[:, :, :3])
+
+        # crop top 50px from image
         image = transforms.functional.crop(image, 50, 0, 174, 224)
 
+        # transform input image
         preprocess = transforms.Compose([
             transforms.Resize(size=(self.width, self.height)),
-            # transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
